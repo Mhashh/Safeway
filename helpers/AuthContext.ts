f269ab3a9be914c29cbd23845e8e1411c1c3443c
Mhashh <m04tiria@gmail.com>
2023-06-24 18:09:27 +0530

    removed cyclic depe.

---
+++ helpers/AuthContext.ts
@@ -0,0 +1,28 @@
+import { Client } from "@hashgraph/sdk";
+import React from "react";
+import { LatLng } from "react-native-maps";
+
+export type GlobalState = {
+    client:Client,
+    setClient:React.Dispatch<React.SetStateAction<Client>>,
+    useracc:string,
+    setUserAcc:React.Dispatch<React.SetStateAction<string>>
+  }
+
+  export type RootStackParamList = {
+    Main: {
+    },
+    MapShowContract:{
+      polygon:LatLng[],
+      hits:LatLng[]|undefined
+    },
+    DetectHits:{
+      polygon:LatLng,
+      contractid:string
+    },
+    Login:{
+  
+    }
+  };
+
+export const AuthContext = React.createContext<GlobalState>(undefined);
