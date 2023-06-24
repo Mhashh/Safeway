3d723c8bded27644a4ff25b648114308051b4658
Mhashh <m04tiria@gmail.com>
2023-06-24 18:11:02 +0530

    removed cyclic depe.

--- NavigationView.tsx
+++ NavigationView.tsx
@@ -2,41 +2,69 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 //import MapDisplay from './userscreens/MapDisplay';
-import { AuthContext } from './App';
+
 import MainDisplay from './userscreens/Main';
+import Login from './userscreens/Login';
+import ShowThisContract from './userscreens/ShowThisContract';
+import { Client } from '@hashgraph/sdk';
+import { AuthContext, RootStackParamList } from './helpers/AuthContext';
 
-export type RootStackParamList = {
-  Main: {
-  },
-  Map:{
 
-  },
-  Login:{
-
-  }
-};
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
  
 
 const MyStack = () => {
-  const client = React.useContext(AuthContext)
+
+  const [client,setClient] = React.useState<Client>(undefined);
+  const [useracc,setUserAcc] = React.useState<string>("");   
   return (
+    <AuthContext.Provider  value={{client:client,setClient:setClient,useracc,setUserAcc}}>
     <NavigationContainer>
-      
-        
+
+      {client === undefined?
+        <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          initialParams={{  }}
+        />
+
+        </Stack.Navigator>
+      :
         <Stack.Navigator>
         <Stack.Screen
           name="Main"
           component={MainDisplay}
           initialParams={{  }}
         />
+        <Stack.Screen
+          name="MapShowContract"
+          component={ShowThisContract}
+          initialParams={{  }}
+        />
 
         </Stack.Navigator>
-        
+      }       
     </NavigationContainer>
+    </AuthContext.Provider>
     
   );
 };
 
 export default MyStack;
+
+/*
+{
+          <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            initialParams={{  }}
+          />
+  
+          </Stack.Navigator>
+
+      }else 
+      
+      */
