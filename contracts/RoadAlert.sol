02287ae40357aa71a6b6daa01342c103567a06ac
Mhashh <m04tiria@gmail.com>
2023-07-05 02:33:37 +0530

    added missing update for alert count and rate conv

--- contracts/RoadAlert.sol
+++ contracts/RoadAlert.sol
@@ -29,18 +29,24 @@ contract RoadAlert{
     //contributors
     function detected(int32 long,int32 lat) external {
 
-        require((hits<100)&&(long<=180000000 && long>=-90000000 && lat<=180000000 && lat>=-90000000));
+        require((hits<50)&&(long<=180000000 && long>=-180000000 && lat<=90000000 && lat>=-90000000));
         //checking nearby detected points ~ > 10m distance
         for(uint i = 0;i<hits;i++){
             require((longitude[i]+latitude[i]-long-lat) >= 198,"too close");
                         
         }
+
+        longitude[hits] = long;
+        latitude[hits] = lat;
+        hits = hits+1;
+
         if(msg.sender != owner){
              (bool success2,bytes memory result2) = price.call(abi.encodeWithSignature("getData()"));
-
+            
             require(success2,"unable to convert");
+            
             (,int256 answer, ,) = abi.decode(result2,(uint,int256,uint80,uint8));
-
+            
             payable(msg.sender).transfer((amount_per_hit/uint256(answer))*10**8);
         }
         
@@ -48,6 +54,7 @@ contract RoadAlert{
     }
 
     function remove(uint16 index) external{
+        
         require(msg.sender == owner);
         if( index < hits ){
             //checking nearby detected points ~ > 10m distance
@@ -66,7 +73,14 @@ contract RoadAlert{
     }
 
     function viewMarkers() external payable{
-        require(msg.value == viewcost,"Payment not full.");
+        (bool success2,bytes memory result2) = price.call(abi.encodeWithSignature("getData()"));
+            
+        require(success2,"unable to convert");
+        
+        (,int256 answer, ,) = abi.decode(result2,(uint,int256,uint80,uint8));
+
+
+        require(msg.value >= ((viewcost/uint256(answer))*10**8),"Payment not full.");
         (bool success,bytes memory result) = dev.call(abi.encodeWithSignature("viewrate()"));
         require(success,"no viewrate");
         
@@ -78,7 +92,8 @@ contract RoadAlert{
     }
 
     function viewMarker(uint index) external  view returns(int32 ,int32 ){
-        uint time = (viewer[msg.sender]  - block.timestamp)/86400;
+        //time available
+        uint time = ( block.timestamp - viewer[msg.sender])/86400;
         require(time<=1,"ended");
         return(longitude[index],latitude[index]);
     }
